2f5d2d98ed53b9a9c172fee26315184d97cd87a7
tegocalderonn <1DAM@A11-PC19>
2025-11-19 13:53:34 +0100

    Llamada de funciones 1 y 2

--- Reto3DanielMarcosHector/src/reto3/Reto3.java
+++ Reto3DanielMarcosHector/src/reto3/Reto3.java
@@ -3,6 +3,12 @@ package reto3;
 public class Reto3 {
 
 	public static void main(String[] args) {
+		// Funciones 1 -> HectorH
+		System.out.println(Funciones1.capitalizar("hOLA"));
+		System.out.println(Funciones1.generarApodo("Juan"));
+		
+		System.out.println("====");
+		
 		// Funciones 2 -> DanielO
 		System.out.println(Funciones2.contarPalabras("hola que tal"));
 		System.out.println(Funciones2.invertirFrase("me llamo Dani"));
