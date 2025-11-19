d4a835561e502921c740f3e3b01a78e7dfa907cc
tegocalderonn <1DAM@A11-PC19>
2025-11-19 13:39:50 +0100

    Funcion 1 -> mostrar cuantas palabras tiene una frase. Funcion 2 -> darle la vuelta a una palabra.

--- Reto3DanielMarcosHector/src/reto3/Funciones2.java
+++ Reto3DanielMarcosHector/src/reto3/Funciones2.java
@@ -1,5 +1,35 @@
 package reto3;
 
 public class Funciones2 {
+	public static int contarPalabras(String frase) {
+		int contador = 0; // Inicializacion del contador.
+		for (int i = 0; i < frase.length(); i++) {
+			char c = frase.charAt(i);
+			if (Character.isWhitespace(c)) { // Cuando detecta un espacio en blanco recorriendo la palabra, suma al
+												// contador.
+				contador++; // Suma de valores.
+			}
+		}
+		return contador + 1; // Añadimos un numero mas al final para que salga el numero correcto de
+								// palabras.
+	}
+
+	public static String invertirFrase(String frase) {
+		String palabra = "";
+		String inversa = "";
+
+		for (int i = 0; i < frase.length(); i++) {
+			char c = frase.charAt(i);
+			if (Character.isWhitespace(c)) {
+				inversa = palabra + " " + inversa;
+				palabra = "";
+			} else {
+				palabra += c;
+			}
+		}
+		inversa = palabra + " " + inversa;
+
+		return inversa;
+	}
 
 }
--- Reto3DanielMarcosHector/src/reto3/Reto3.java
+++ Reto3DanielMarcosHector/src/reto3/Reto3.java
@@ -3,8 +3,6 @@ package reto3;
 public class Reto3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
