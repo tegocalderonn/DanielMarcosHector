756cd27d067c3f678b10b100b203665eaa3f3703
abenia26 <1DAM@A11-PC27>
2025-11-19 13:27:29 +0100

    fUNCION CAPITALIZAR Y GENERAR APODO

--- Reto3DanielMarcosHector/src/reto3/Funciones1.java
+++ Reto3DanielMarcosHector/src/reto3/Funciones1.java
@@ -1,5 +1,27 @@
 package reto3;
 
-public class Funciones1 {
+import java.util.Scanner;
 
+public class Funciones1 {
+	
+	public static String capitalizar(String s) {
+	String minusculas=s.toLowerCase();
+	String mayusculas=s.substring(0,1).toUpperCase();
+	String restominusculas=minusculas.substring(1);
+	return mayusculas+restominusculas;
+	}
+	public static String generarApodo(String palabra) {
+		String primerastres=palabra.substring(0,3);
+		String ultimaletra=palabra.substring(palabra.length()-1);
+		return primerastres+ultimaletra+palabra.length();
+	}
+	public static void cadenaVacia(Scanner sc) {
+		String palabra = "";
+		do {
+			System.out.println("Dime un nombre");
+			palabra = sc.nextLine();
+			
+		} while (palabra.length() != 0);
+
+	}
 }
