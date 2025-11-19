8286a7a70b93466cf83f725aed279248fcda6a2c
tegocalderonn <1DAM@A11-PC19>
2025-11-19 13:40:19 +0100

    Funcion 1 -> mostrar cuantas palabras tiene una frase. Funcion 2 -> darle la vuelta a una palabra.

--- Reto3DanielMarcosHector/src/reto3/Funciones2.java
+++ Reto3DanielMarcosHector/src/reto3/Funciones2.java
@@ -5,31 +5,29 @@ public class Funciones2 {
 		int contador = 0; // Inicializacion del contador.
 		for (int i = 0; i < frase.length(); i++) {
 			char c = frase.charAt(i);
-			if (Character.isWhitespace(c)) { // Cuando detecta un espacio en blanco recorriendo la palabra, suma al
-												// contador.
-				contador++; // Suma de valores.
+			if (Character.isWhitespace(c)) { // Cuando detecta un espacio en blanco
+				contador++; // Suma los espacios en blanco (si hay dos palabras habria un espacio en blanco*.
 			}
 		}
-		return contador + 1; // Añadimos un numero mas al final para que salga el numero correcto de
-								// palabras.
+		return contador + 1; // *entonces si dos palabras= unespacio en blanco, sumo uno, por ello saldria que habria 2 palabras
 	}
 
 	public static String invertirFrase(String frase) {
-		String palabra = "";
-		String inversa = "";
+		String palabra = ""; // reseteamos la palabra para que no de error lenght
+		String inversa = ""; // reseteamos la palabra para que no de error lenght
 
-		for (int i = 0; i < frase.length(); i++) {
+		for (int i = 0; i < frase.length(); i++) { // for que recorre toda la palabra
 			char c = frase.charAt(i);
-			if (Character.isWhitespace(c)) {
-				inversa = palabra + " " + inversa;
-				palabra = "";
+			if (Character.isWhitespace(c)) { // Si hay espacio en blanco
+				inversa = palabra + " " + inversa; // la inversa sera la palabra con inversa
+				palabra = ""; // reseteo palabra para que se ponga la siguiente despues del espacio
 			} else {
-				palabra += c;
+				palabra += c; // las palabras de la palabrase suman
 			}
 		}
-		inversa = palabra + " " + inversa;
+		inversa = palabra + " " + inversa; 
 
-		return inversa;
+		return inversa; // fin
 	}
 
 }
